43e359cf789066b2f7e5a5c65b4379c86c6dbd66
Apstena <79761048+Apstena@users.noreply.github.com>
2021-05-22 17:50:45 +0700

    Update FP_test.py

--- FP_test.py
+++ FP_test.py
@@ -308,7 +308,7 @@ ods_mdm_user = PostgresOperator(
           '2999-12-31 00:00:00'::timestamp as exp_dttm ,
           0 as del_ind
           from adubinsky.fp_v_mdm_ods_mdm_user v
-          where xists (select 1 from adubinsky.fp_ods_mdm_user v where v.user_ud=m.user_id and (m.hashsum!=v.hashsum or m.del_ind=1)
+          where exists (select 1 from adubinsky.fp_ods_mdm_user v where v.user_ud=m.user_id and (m.hashsum!=v.hashsum or m.del_ind=1)
                         and m.exp_dttm='2999-12-31 00:00:00'::timestamp)
           ;
 
